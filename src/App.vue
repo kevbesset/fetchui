2a05e9329300ca24483e2569ede4dbf5588fe5f1
Kevin <kevbesset@gmail.com>
2025-12-17 15:38:57 +0100

    feat(app): 🎨 add Button component demo showcase

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/ui/buttons/Button.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="demo">
+    <h2>Buttons <Button icon-only>+</Button></h2>
+    <div class="list">
+      <Button>Button</Button>
+      <Button variant="primary">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="discrete">Button</Button>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss">
-@use './styles/main.scss';
+<style lang="scss">
+@use '@/styles/main';
+
+.demo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--gap);
+  padding-inline: var(--spacingInline);
+  height: 100dvh;
+}
+
+.list {
+  display: flex;
+  gap: var(--gap);
+}
 </style>
