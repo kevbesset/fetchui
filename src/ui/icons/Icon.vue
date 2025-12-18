4afba1e3c3f7bbbe975ecc9dcf7e839128ce7256
Kevin <kevbesset@gmail.com>
2025-12-18 12:25:13 +0100

    feat(ui): ✨ add Icon component with Iconify integration

---
+++ src/ui/icons/Icon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Icon as IconifyIcon } from '@iconify/vue'
+
+withDefaults(
+  defineProps<{
+    name: string
+    collection?: string
+  }>(),
+  {
+    collection: 'mingcute',
+  },
+)
+</script>
+
+<template>
+  <IconifyIcon :icon="`${collection}:${name}`" aria-hidden="true" class="icon" />
+</template>
+
+<style scoped lang="scss">
+.icon {
+  vertical-align: middle;
+}
+</style>
